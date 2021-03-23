b1917d337c59a90d8914ece24d35cc7753e225b6
Sebastian Kähkönen <seebbe_@hotmail.com>
2021-03-23 19:08:30 +0100

    Fixed a shell for our project

---
+++ the-game/src/main/java/group1/horrorgame/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package group1.horrorgame.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
