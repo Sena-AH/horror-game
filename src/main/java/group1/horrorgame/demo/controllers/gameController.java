fc6d2172ce945e3052237f9e94687fd5c59bd44b
Sena A.H <sena@Senas-Air.lan>
2021-04-06 16:58:18 +0200

    Added some annotations in Controller

--- src/main/java/group1/horrorgame/demo/controllers/gameController.java
+++ src/main/java/group1/horrorgame/demo/controllers/gameController.java
@@ -1,4 +1,18 @@
 package group1.horrorgame.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class gameController {
+
+    @Autowired
+    ItemService itemService;
+
+    @Autowired
+    PlayerService playerService;
+
+    @Autowired
+    VillainService villainService;
+
 }
