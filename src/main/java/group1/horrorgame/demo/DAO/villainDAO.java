2d3089151e3167cef66d9e9423ce480a3b688c97
JuliB-dotcom <69867011+JuliB-dotcom@users.noreply.github.com>
2021-04-09 14:34:49 +0200

    Putted some basic content in Controller, DAO and Service class

--- src/main/java/group1/horrorgame/demo/DAO/villainDAO.java
+++ src/main/java/group1/horrorgame/demo/DAO/villainDAO.java
@@ -1,4 +1,10 @@
 package group1.horrorgame.demo.DAO;
 
-public class villainDAO {
+
+public class VillainDAO {
+    /*private final villainRepository repository;*/
 }
+   /* public VillainDAO(villainRepository repository) {
+        this.repository = repository;
+    }*/
+
--- src/main/java/group1/horrorgame/demo/services/villainService.java
+++ src/main/java/group1/horrorgame/demo/services/villainService.java
@@ -1,4 +1,14 @@
 package group1.horrorgame.demo.services;
 
-public class villainService {
+import group1.horrorgame.demo.DAO.VillainDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VillainService {
+   /* private final VillainDAO villainDAO;*/
+/*
+@Autowired
+public VillainService(VillainDAO villainDAO) {
+    this.villainDAO = villainDAO;*/
 }
