fae2fd6e0fc9be76dca242510235846bcbe2b505
niklas <NiklasKullberg@mail.com>
2021-04-10 15:25:49 +0200

    + Started work om visuals for fight-1 vs Samara, inlcuding placement and size of game play frame

--- src/main/java/group1/horrorgame/demo/controllers/gameController.java
+++ src/main/java/group1/horrorgame/demo/controllers/gameController.java
@@ -3,12 +3,24 @@ package group1.horrorgame.demo.controllers;
 import group1.horrorgame.demo.services.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class gameController {
 //
     @Autowired
     ItemService itemService;
+
+    @RequestMapping("/index")
+    public String start()   {
+        return "index";
+    }
+
+    @RequestMapping("/fight-1")
+    public String startFight1() {
+        return "fight-1";
+    }
+
 //
 //    @Autowired
 //    PlayerService playerService;
