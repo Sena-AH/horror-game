41bc06320a53773ae25fe95498d37bf88b97b88a
Sebastian Kähkönen <seebbe_@hotmail.com>
2021-04-11 12:51:30 +0200

    Created player.html and added a baselayer(picture) to it, and connected html to hash(localhost:8080/player)

--- src/main/java/group1/horrorgame/demo/controllers/gameController.java
+++ src/main/java/group1/horrorgame/demo/controllers/gameController.java
@@ -1,17 +1,24 @@
 package group1.horrorgame.demo.controllers;
 
 import group1.horrorgame.demo.services.ItemService;
+import group1.horrorgame.demo.services.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class gameController {
-//
+
     @Autowired
     ItemService itemService;
-//
-//    @Autowired
-//    PlayerService playerService;
+
+    @Autowired
+    PlayerService playerService;
+    @RequestMapping("player")
+    public String showPlayers(Model model){
+        return "player";
+    }
 //
 //    @Autowired
 //    VillainService villainService;
