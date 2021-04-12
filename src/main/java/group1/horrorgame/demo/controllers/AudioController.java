1821b6b993cd7371d661d620711eadb3d950783d
JuliB-dotcom <69867011+JuliB-dotcom@users.noreply.github.com>
2021-04-12 14:19:01 +0200

    Added a AudioController and added audio autoplay to index

---
+++ src/main/java/group1/horrorgame/demo/controllers/AudioController.java
@@ -0,0 +1,7 @@
+package group1.horrorgame.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+@Controller
+public class AudioController {
+   public static String uploadDirectory=
+}
