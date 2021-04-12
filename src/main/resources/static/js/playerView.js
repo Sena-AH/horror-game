72f24f85eec5b535df8a34a5c5b49889725737e1
Sebastian Kähkönen <seebbe_@hotmail.com>
2021-04-12 14:58:52 +0200

    (HTML,CSS,Javascript)Added styling, pictures, eventlisteners for the buttons and some functions to keep the story going

---
+++ src/main/resources/static/js/playerView.js
@@ -0,0 +1,30 @@
+document.getElementById("arrDown").addEventListener("click", down);
+document.getElementById("arrLeft").addEventListener("click", left);
+document.getElementById("arrRight").addEventListener("click", right);
+
+
+
+function down(){
+    console.log("down");
+}
+function left(){
+    let imageSet = document.getElementById("baseImage").src;
+    console.log(imageSet);
+    if(imageSet.includes("doorScreen.jpg")){
+        document.getElementById("arrLeft").addEventListener("click", function(){
+            document.getElementById("baseImage").src="images/jigsawScreen.jpg";
+            document.getElementById("arrDown").remove();
+            document.getElementById("arrLeft").remove();
+            document.getElementById("arrRight").remove();
+        })
+    }
+}
+function right(){
+    let imageSet = document.getElementById("baseImage").src;
+    if(imageSet.includes("BaseScreen.jpg")){
+        console.log("thisIsBaseScreen");
+        document.getElementById("baseImage").src="images/doorScreen.jpg";
+        document.getElementById("arrLeft").style.transform = "rotate(90deg)";
+        document.getElementById("arrRight").style.transform = "rotate(90deg)";
+    }
+}
