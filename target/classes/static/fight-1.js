d92e74d80b5ff12b373ea33d0064583d7f6548fa
niklas <NiklasKullberg@mail.com>
2021-04-13 19:49:50 +0200

    changes merged from main

---
+++ target/classes/static/fight-1.js
@@ -0,0 +1,20 @@
+
+
+
+$(function ()   {
+
+    let $textBox = $('#text-box');
+
+
+
+    function startFight ()  {
+        $textBox.html(
+            '<p>A wild Samara appeared<br></p>' +
+            '<p>You cant escape me, i will haunt you forever</p>'
+        );
+    }
+
+    startFight();
+
+
+});
