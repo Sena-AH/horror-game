1e6e0adb9c4a89769afc3d056b16c94d3bf52279
JuliB-dotcom <69867011+JuliB-dotcom@users.noreply.github.com>
2021-04-13 16:46:52 +0200

    Removal of AudioController added js

--- src/main/java/group1/horrorgame/demo/controllers/AudioController.java
+++
@@ -1,7 +0,0 @@
-package group1.horrorgame.demo.controllers;
-
-import org.springframework.stereotype.Controller;
-@Controller
-public class AudioController {
-   public static String uploadDirectory=
-}
