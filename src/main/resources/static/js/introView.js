2a7de125860535235580ca001627496f1f8532fa
JuliB-dotcom <69867011+JuliB-dotcom@users.noreply.github.com>
2021-04-14 10:02:41 +0200

    Updated js-file and html.

--- src/main/resources/static/js/introView.js
+++ src/main/resources/static/js/introView.js
@@ -1,45 +1,74 @@
+$('#introBase2Img').hide();
+$('#introText2').hide();
+$('#introText3').hide();
+$('#introText4').hide();
+$('#doorIntroText').hide();
+$('#doorIntroImg').hide();
+$('#gameStartText').hide();
+$('#gameStartImg').hide();
 
-document.getElementById("introPart2").style.display="none";
+/*document.getElementById("introPart2").style.display="none";
 document.getElementById("introPart3").style.display="none";
 document.getElementById("introPart4").style.display="none";
 document.getElementById("doorIntro").style.display="none";
 document.getElementById("gameStart").style.display="none";
 document.getElementById("gameStartImg").style.display="none";
 document.getElementById("doorIntroImg").style.display="none";
-document.getElementById("IntroBase2").style.display="none";
+document.getElementById("IntroBase2").style.display="none";*/
 showText();
 
 
 
 function showText(){
 
-    let Img1 = document.getElementById("baseImage").src;
-    let Img2 = document.getElementById("IntroBase2").src;
+    let baseImg1 = document.getElementById("introBaseImg").src;
+    let baseImg2 = document.getElementById("introBase2Img").src;
+    let baseImg3 = document.getElementById("gameStartImg").src;
     /*if(onclick*/
 
-    document.getElementById("introPart2").style.display="block";
-    document.getElementById("introPart1").style.display="none";
+    $('introText1').hide();
+    $('introText2').show();
 
-    if(Img1.includes("introPart2")) {
-        document.getElementById("introPart3").style.display="block";
-        document.getElementById("introPart2").style.display="none";
+
+   /* document.getElementById("introText2").style.display="block";
+    document.getElementById("introText1").style.display="none";
+*/    if(baseImg1.includes("introText2")) {
+        $('introText2').hide();
+        $('introText3').show();
+
+       /* document.getElementById("introText3").style.display="block";
+        document.getElementById("introText2").style.display="none";*/
     }
-    if(Img1.includes("introPart3")) {
-        document.getElementById("IntroBase2").style.display="block";
-        document.getElementById("introPart4").style.display="block";
-        document.getElementById("IntroBase").style.display="none";
-        document.getElementById("introPart3").style.display="none";
+    if(baseImg1.includes("introText3")) {
+        $('introBaseImg').hide();
+        $('introText3').hide();
+        $(baseImg2).show();
+        $('introText4').show();
+
+        /*document.getElementById("IntroBase2Img").style.display="block";
+        document.getElementById("introText4").style.display="block";
+        document.getElementById("IntroBaseImg").style.display="none";
+        document.getElementById("introText3").style.display="none";*/
     }
-    if(Img2.includes("introPart4")) {
-        document.getElementById("gameStartImg").style.display="block";
-        document.getElementById("gameStart").style.display="block";
-        document.getElementById("introPart4").style.display="none";
+    if(baseImg2.includes("introText4")) {
+        $(baseImg2).hide();
+        $('introText4').hide();
+        $(baseImg3).show();
+        $('gameStartText').show();
+
+        /*document.getElementById("gameStartImg").style.display="block";
+        document.getElementById("gameStartText").style.display="block";
+        document.getElementById("introText4").style.display="none";*/
     }
 
-    if(Img2.includes("gameStart")) {
-        document.getElementById("doorIntroImg").style.display="block";
-        document.getElementById("doorIntro").style.display="block";
-        document.getElementById("gameStart").style.display="none";
+    if(baseImg3.includes("gameStartText")) {
+        $(baseImg3).hide();
+        $('gameStartText').hide();
+        $('doorIntroImg').show();
+        $('doorIntroText').show();
+        /*document.getElementById("doorIntroImg").style.display="block";
+        document.getElementById("doorIntroText").style.display="block";
+        document.getElementById("gameStartImg").style.display="none";*/
     }
   /*  if(onclick) {*/
         /*Till sist. Klickar du på den sista bilden så hänvisas du till en ny sida.*/
