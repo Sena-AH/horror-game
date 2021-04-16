5c32474098a3109901fcab7274ad8b1a1ba059e4
niklas <NiklasKullberg@mail.com>
2021-04-16 12:26:09 +0200

    + adding buttons samara-fight

--- src/main/resources/static/js/samara.js
+++ src/main/resources/static/js/samara.js
@@ -3,5 +3,8 @@
 
 setTimeout(function () {
     document.getElementById("samaraBase").src="/images/fight_1_samara.jpg";
+    document.getElementById("statsAndButtons").append(
+        "<button id=\"fightButton\"><img th:src=\"@{/images/fight_button.jpg}\"></button>"
+    );
 }, 3000);
 
