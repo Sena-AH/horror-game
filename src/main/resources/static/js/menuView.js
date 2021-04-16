3559bb2371cbbebe03621a5aed99bb29fd0c0cb2
JuliB-dotcom <69867011+JuliB-dotcom@users.noreply.github.com>
2021-04-16 15:19:00 +0200

    Updated buttons for startMenu and made them clickable

--- src/main/resources/static/js/menuView.js
+++ src/main/resources/static/js/menuView.js
@@ -1,18 +1,34 @@
 
-let menuSound = new sound("/mp3/menuSound.mp3");
-menuSound.play();
+document.getElementById("resumeButton").addEventListener("click", resumeButton);
+document.getElementById("startButton").addEventListener("click", startButton);
+document.getElementById("exitButton").addEventListener("click", exitButton);
+
+
+function startButton(){
+        location.href = "intro";
+    }
+
+function resumeButton() {
+    location.href = "player";
 }
 
-function sound(src){
+function exitButton() {
+    location.href = "player";
+}
+function sound(src) {
     this.sound = document.createElement("audio");
     this.sound.src = src;
     this.sound.setAttribute("preload", "auto");
     this.sound.setAttribute("controls", "none");
     this.sound.style.display = "none";
     document.body.appendChild(this.sound);
-    this.play = function(){
+    this.play = function () {
         this.sound.play();
     }
-    this.stop = function(){
+    this.stop = function () {
         this.sound.pause();
     }
+
+    let menuSound = new sound("/mp3/menuSound.mp3");
+    menuSound.play();
+}
