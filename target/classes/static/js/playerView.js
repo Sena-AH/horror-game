9b73bc44afba509bd810b25490996794f373364b
Sebastian Kähkönen <seebbe_@hotmail.com>
2021-04-17 11:08:27 +0200

    Added backgroundMusic

--- target/classes/static/js/playerView.js
+++ target/classes/static/js/playerView.js
@@ -1,39 +1,47 @@
+let backgroundMusic = new sound("/mp3/backgroundMusic.mp3");
 document.getElementById("arrDown").addEventListener("click", down);
 document.getElementById("arrLeft").addEventListener("click", left);
 document.getElementById("arrRight").addEventListener("click", right);
 
-
+function startmusic(){
+    backgroundMusic.play();
+}
 
 function down(){
+    // backgroundMusic.play();
     console.log("down");
 }
 function left(){
-    let imageSet = document.getElementById("baseImage").src;
-    console.log(imageSet);
-
+    startmusic()
     $('.baseDiv').empty();
     $('.baseDiv').append('<img class="doorScreenImage"src="images/doorScreen.jpg">');
     $('.baseDiv').append('<button class="enterLeftRoomButton" onclick=changeLocation("chucky")></button>')
     $('.baseDiv').append('<button class="enterRightRoomButton" onclick=changeLocation("notDone")></button>')
-
-    // if(imageSet.includes("doorScreen.jpg")){
-    //     location.href = "jigsaw";
-    // }
 }
 function right(){
-    // let imageSet = document.getElementById("baseImage").src;
-    // if(imageSet.includes("BaseScreen.jpg")){
-        $('.baseDiv').empty();
-        $('.baseDiv').append('<img class="doorScreenImage"src="images/doorScreen.jpg">');
-        $('.baseDiv').append('<button class="enterLeftRoomButton" onclick=changeLocation("jigsaw")></button>')
-        $('.baseDiv').append('<button class="enterRightRoomButton" onclick=changeLocation("notDone")></button>')
-
-    // }
-    // if(imageSet.includes("BaseScreen.jpg")){
-    //
-    // }
+    startmusic()
+    $('.baseDiv').empty();
+    $('.baseDiv').append('<img class="doorScreenImage"src="images/doorScreen.jpg">');
+    $('.baseDiv').append('<button class="enterLeftRoomButton" onclick=changeLocation("jigsaw")></button>')
+    $('.baseDiv').append('<button class="enterRightRoomButton" onclick=changeLocation("notDone")></button>')
 }
 
 function changeLocation(thePath){
+    startmusic()
     location.href= thePath;
+}
+
+function sound(src){
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+        this.sound.play();
+    }
+    this.stop = function(){
+        this.sound.pause();
+    }
 }
