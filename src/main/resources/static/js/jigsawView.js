cb265c832b69f4afd1820ac1f84f4e14ea05a8df
Sebastian Kähkönen <seebbe_@hotmail.com>
2021-04-19 20:46:52 +0200

    Huge push(forgot to push), done with jigsawGame, kept going on jigsawScreens(javascript/css)

--- src/main/resources/static/js/jigsawView.js
+++ src/main/resources/static/js/jigsawView.js
@@ -17,6 +17,9 @@ function startJigsaw(){
     $('.mainDiv').prepend('<button id= "screen1Answer2Btn" class="screen1Answer2" onclick="screenOneAnswerTwo()"></button>');
 }
 
+/*
+* If you pick answer one you go this path with functions
+* */
 function screenOneAnswerOne(){
     $('.mainDiv').empty();
     $('.mainDiv').append('<img class="answer1Screen2" src="images/jigsawFight/jigsawScreenAnswer1Screen2.jpg">');
@@ -39,22 +42,41 @@ function nextChatBubble(){
 
 function nextGameScreen(){
     $('.mainDiv').empty();
-    setTimeout(gameOver, 30000);
+    setTimeout(gameOver, 60000);
     $('.mainDiv').append('<img class="jigsawGameScreenOne" src="/images/jigsawFight/jigsawGameScreen3.jpg">')
     $('.mainDiv').append('<button class="jigsawGameClueNumberOne" onclick=clueOne()></button>')
     $('.mainDiv').append('<button class="jigsawGameClueNumberTwo" onclick=clueTwo()></button>')
     $('.mainDiv').append('<button class="jigsawGameClueNumberThree" onclick=clueThree()></button>')
-    $('.mainDiv').append('<button class="enterKeycodeButton" onclick=enterKeycode()></button>')
+    $('.mainDiv').append('<button id="enterKeycodeBtn" class="enterKeycodeButton" onclick=enterKeycode()></button>')
 }
 
 function enterKeycode(){
-    $('.enterKeycodeButton').remove();
-    $('.mainDiv').append('<button class="keyCodeSubmit" onclick=keycodeGenerator()></button>')
+    if(document.getElementById("keycodePic") != null){
+        document.getElementById("keycodePic").remove();
+        document.getElementById("keycodeInputVal").remove();
+        document.getElementById("submitBtn").remove();
+    }
+    else{
+        $('.mainDiv').append('<img id="keycodePic" class="keycodeGenerator"src="images/jigsawFight/jigsawKeycodeGenerator.jpg">');
+        $('.mainDiv').append('<input class="submitInput" id="keycodeInputVal" class="keycodeInputValue" maxlength="6">')
+        $('.mainDiv').append('<button id="submitBtn" class="submitButton" onclick=checkVal()>Enter</button>')
+    }
 }
 
-function keycodeGenerator(){
-    $('.mainDiv').append();
+function checkVal(){
+    if(document.getElementById("keycodeInputVal").value === "452395"){
+        $('.mainDiv').empty();
+        $('.mainDiv').append('<img class="answer1Screen2" src="/images/jigsawFight/jigsawYouHaveWon.jpg">')
+        setTimeout(function(){
+            $('.mainDiv').empty();
+            $('.mainDiv').append('<img class="answer1Screen2" src="/images/winner_frame.jpg">')
+        },10000);
+        setTimeout(function(){
+            location.href="player"
+        }, 15000)
+    }
 }
+
 
 function clueOne(){
     $('.clueOne').remove();
@@ -78,9 +100,60 @@ function gameOver(){
     $('.mainDiv').append('<img class="jigsawGameScreenOne" src="/images/jigsawFight/gameOverScreen.jpg">');
 }
 
+
+
+/*
+* If you pick answer one you go this path with functions
+* */
 function screenOneAnswerTwo(){
-    console.log("this is oneTWO")
+    $('.mainDiv').empty();
+    $('.mainDiv').append('<img class="answer1Screen2" src="images/jigsawFight/jigsawScreen2Answer2.jpg">');
+    $('.mainDiv').append('<button id="question1Btn" class="question1Button" onclick="screenTwoAnswerOne()"></button>')
+    $('.mainDiv').append('<button id="question2Btn" class="question2Button" onclick="screenTwoAnswerTwo()"></button>')
+    $('.mainDiv').append('<button id="question3Btn" class="question3Button" onclick="screenTwoAnswerThree()"></button>')
 }
+
+function screenTwoAnswerOne(){
+    $('.mainDiv').empty();
+    $('.mainDiv').append('<img class="answer1Screen2" src="images/jigsawFight/jigsawAnswer1Screen3.jpg">');
+    $('.mainDiv').append('<button id="question1BtnScreen3" class="question1ButtonScreen3" onclick="screenThreeAnswerOne()"></button>')
+    $('.mainDiv').append('<button id="question2BtnScreen3" class="question2ButtonScreen3" onclick="screenThreeAnswerTwo()"></button>')
+    $('.mainDiv').append('<button id="question3BtnScreen3" class="question3ButtonScreen3" onclick="screenThreeAnswerThree()"></button>')
+}
+function screenTwoAnswerTwo(){
+    $('.mainDiv').append('<img class="answer1Screen2" src="images/jigsawFight/jigsawAnswer2Screen3.jpg">');
+}
+function screenTwoAnswerThree(){
+    location.href="player";
+}
+
+
+function screenThreeAnswerOne(){
+
+}
+function screenThreeAnswerTwo(){
+    $('.mainDiv').empty();
+    $('.mainDiv').append('<img class="answer1Screen2" src="images/jigsawFight/jigsawAnswer2Screen4.jpg">');
+    $('.mainDiv').append('<button id="question1BtnScreen4" class="question1ButtonScreen4" onclick="questionOneScreenFour()"></button>')
+    $('.mainDiv').append('<button id="question2BtnScreen4" class="question2ButtonScreen4" onclick="questionTwoScreenFour()"></button>')
+}
+function screenThreeAnswerThree(){
+
+}
+function questionOneScreenFour(){
+    $('.mainDiv').empty();
+    $('.mainDiv').append('<img class="answer1Screen2" src="images/jigsawFight/jigsawScreenFive.jpg">');
+}
+function questionTwoScreenFour(){
+    $('.mainDiv').empty();
+    $('.mainDiv').append('<img class="answer1Screen2" src="images/jigsawFight/jigsawScreenFourAnswerTwo.jpg">');
+    $('.mainDiv').append('<button id="enterRoomBtn" class="enterRoomButton" onclick=changeLocation("player")></button>')
+}
+
+function changeLocation(thePath){
+    location.href= thePath;
+}
+
 
 function sound(src){
     this.sound = document.createElement("audio");
