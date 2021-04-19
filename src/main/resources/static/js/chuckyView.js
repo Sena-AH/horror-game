e048dc65220ebb22d0125dbcf6363694f17900db
Sena A.H <sena@Senas-Air.lan>
2021-04-19 21:41:07 +0200

    added more frames and buttons to continue the chucky route

--- src/main/resources/static/js/chuckyView.js
+++ src/main/resources/static/js/chuckyView.js
@@ -75,7 +75,66 @@ function actButtonPress(){
     $('.chuckymain').append('<button class="chuckyactBtn actpageactBtn" id="actButton" onclick="actButtonPress()">act</button>');
     $('.chuckymain').append('<button class="chuckyescapeBtn actpageescapeBtn" id="escapeButton" onclick="escapeButtonPress()">escape</button>');
 
+    $('.chuckymain').append('<button class="act1" id="act1" onclick="act1()">act1</button>');
+    $('.chuckymain').append('<button class="act2" id="act2" onclick="act2()">act2</button>');
+    $('.chuckymain').append('<button class="act3" id="act3" onclick="act3()">act3</button>');
 }
+
+function act1() {
+    $('.chuckymain').empty();
+    $('.chuckymain').append('<img src="/images/act1.jpg">');
+    $('.chuckymain').append('<button class="chuckyitemBtn actpageitemBtn" id="itemButton" onclick="itemButtonPress()">items</button>');
+    $('.chuckymain').append('<button class="chuckyactBtn actpageactBtn" id="actButton" onclick="actButtonPress()">act</button>');
+    $('.chuckymain').append('<button class="chuckyescapeBtn actpageescapeBtn" id="escapeButton" onclick="escapeButtonPress()">escape</button>');
+}
+function act2() {
+    $('.chuckymain').empty();
+    $('.chuckymain').append('<img src="/images/act2.jpg">');
+    $('.chuckymain').append('<button class="chuckyitemBtn actpageitemBtn" id="itemButton" onclick="itemButtonPress()">items</button>');
+    $('.chuckymain').append('<button class="chuckyactBtn actpageactBtn" id="actButton" onclick="actButtonPress()">act</button>');
+    $('.chuckymain').append('<button class="chuckyescapeBtn actpageescapeBtn" id="escapeButton" onclick="escapeButtonPress()">escape</button>');
+}
+function act3() {
+    $('.chuckymain').empty();
+    $('.chuckymain').append('<img src="/images/act3.jpg">');
+    $('.chuckymain').append('<button class="chuckyitemBtn actpageitemBtn" id="itemButton" onclick="itemButtonPress()">items</button>');
+    $('.chuckymain').append('<button class="chuckyactBtn actpageactBtn" id="actButton" onclick="act300()">act</button>');
+    $('.chuckymain').append('<button class="chuckyescapeBtn actpageescapeBtn" id="escapeButton" onclick="escapeButtonPress()">escape</button>');
+}
+
+function act300(){
+    $('.chuckymain').empty();
+    $('.chuckymain').append('<img src="/images/act300.jpg">');
+    $('.chuckymain').append('<button class="chuckyitemBtn actpageitemBtn" id="itemButton" onclick="itemButtonPress()">items</button>');
+    $('.chuckymain').append('<button class="chuckyactBtn actpageactBtn" id="actButton" onclick="act300()">act</button>');
+    $('.chuckymain').append('<button class="chuckyescapeBtn actpageescapeBtn" id="escapeButton" onclick="escapeButtonPress()">escape</button>');
+
+    $('.chuckymain').append('<button class="act1" id="act301" onclick="act1()">act1</button>');
+    $('.chuckymain').append('<button class="act2" id="act302" onclick="act302()">act2</button>');
+    $('.chuckymain').append('<button class="act3" id="act303" onclick="act3()">act3</button>');
+}
+
+function act302(){
+    $('.chuckymain').empty();
+    $('.chuckymain').append('<img src="/images/act302.jpg">');
+    $('.chuckymain').append('<button class="youwinbutton" id="youWin" onclick="youWin()">YOU WIN</button>');
+
+}
+
+function youWin(){
+    $('.chuckymain').empty();
+    $('.chuckymain').append('<img src="/images/actwin.jpg">');
+    $('.chuckymain').append('<button class="chuckyactBtn actpageactBtn" id="chuckyContinueButton" onclick="chuckyGone()">continue</button>');
+}
+
+function chuckyGone(){
+    $('.chuckymain').empty();
+    $('.chuckymain').append('<img src="/images/actwinchuckygone.jpg">');
+    $('.chuckymain').append('<button class="doorclick doorclickexit" id="doorClickButton">door</button>');
+    $('.chuckymain').append('<button id="arrDown" class="arrowDown" onclick=changeLocation("player")><img src="@{/images/arrowDown}" width="50x50"></button>');
+
+}
+
 
 function escapeButtonPress(){
     location.href = 'player';
