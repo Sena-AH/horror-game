e3f1ac6688342c10c26d9152c3fccf46402b3d78
niklas <NiklasKullberg@mail.com>
2021-04-20 11:16:07 +0200

    PlayerItemDAO

---
+++ src/main/java/group1/horrorgame/demo/DAO/PlayerItemDAO.java
@@ -0,0 +1,38 @@
+package group1.horrorgame.demo.DAO;
+
+
+import group1.horrorgame.demo.repository.PlayerItemRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class PlayerItemDAO {
+
+    private final PlayerItemRepository playerItemRepository;
+
+    public PlayerItemDAO(PlayerItemRepository playerItemRepository) {
+        this.playerItemRepository = playerItemRepository;
+    }
+
+    public PlayerItemDTO addPlayerItems(PlayerItemDTO playerItemDTO) {
+        return playerItemRepository.save(playerItemDTO);
+    }
+
+    public Iterable<PlayerItemDTO>  getAllPlayerItems() {
+        return playerItemRepository.findAll();
+    }
+
+    public Optional<PlayerItemDTO> findPlayerItemsById(Integer id)  {
+        return playerItemRepository.findById(id);
+    }
+
+    public void deletePlayerItems(Integer id)   {
+        playerItemRepository.deleteById(id);
+    }
+
+    public void deleteAllPlayerItems()  {
+        playerItemRepository.deleteAll();
+    }
+
+}
