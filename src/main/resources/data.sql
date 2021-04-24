db078cf28d89833f1e8d316dc506f85d1d9252a8
Sebastian Kähkönen <seebbe_@hotmail.com>
2021-04-24 13:27:55 +0200

    Full group co-coding, fixed post method, now works fine

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,7 @@
 DELETE FROM item;
 DELETE FROM villains;
 DELETE FROM player_item;
+DELETE FROM players;
 
 INSERT INTO item (id, attack_points, item_name, special_ability) VALUES
 (1, 2, 'priest', 'Uses holy water and chants for extra damage on villains and heals player with one life point'),
@@ -12,6 +13,8 @@ INSERT INTO item (id, attack_points, item_name, special_ability) VALUES
 INSERT INTO villains (id, difficulty, health, special_attack, villain_name, weakness) VALUES
 (1, 7, 10, 'Knife skills', 'Chucky', 'Annabelle');
 
+INSERT INTO players(id, fighter_score, health, level, liar_score, name, pacifist_score, total_score) VALUES
+(3, 10, 3, 'five', 10, 'newname', 3, 2);
 
 INSERT INTO player_item(id, item_name, player_id) VALUES
 (1, 'priest', 1),
